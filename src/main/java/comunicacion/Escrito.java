5a99709b0bb0b0d9788aac9d5f68f73dece8e5b1
Mirestrepot <92109447+Mirestrepot@users.noreply.github.com>
2021-12-07 15:50:20 -0500

    asdf

--- src/main/java/comunicacion/Escrito.java
+++ src/main/java/comunicacion/Escrito.java
@@ -15,18 +15,23 @@ public abstract class Escrito extends Pictograma{
 	public String getTitulo() {
 		return titulo;
 	}
+	
 	public void setTitulo(String titulo) {
 		this.titulo = titulo;
 	}
+
 	public String getAutor() {
 		return autor;
 	}
+
 	public void setAutor(String autor) {
 		this.autor = autor;
 	}
+
 	public int getPaginas() {
 		return paginas;
 	}
+
 	public void setPaginas(int paginas) {
 		this.paginas = paginas;
 	}
